ef92af06ba2eb4fcb16401032cb4806a1c080aec
KuduComp <akoolen@quicknet.nl>
2020-11-04 15:46:29 +0100

    Inital commit :rocket:

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to GC Coolstuff"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+	background-color: #08541d;
+	color: #8ab596;
+}
+</style>
